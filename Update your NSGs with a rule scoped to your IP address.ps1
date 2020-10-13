646b7ca46e0b0457303f9e441c4e478f4164cf5b
Ryan McCallum <rjmccallumbigl@gmail.com>
2020-10-13 16:19:30 -0400

    Prompt for port number
    Accounting for SSH as well as RDP

--- Update your NSGs with a rule scoped to your IP address.ps1
+++ Update your NSGs with a rule scoped to your IP address.ps1
@@ -1,10 +1,13 @@
-# Do you have a policy that blocks RDP if you do not add your IP address to the NSG? Update your NIC and Subnet NSGs with a rule allowing 3389 scoped to your IP address so you can remotely access your VM.
+# Do you have a policy that blocks RDP if you do not add your IP address to the NSG? Update your NIC and Subnet NSGs with a rule allowing 3389 or 22 scoped to your IP address so you can remotely access your VM.
 
 #Enter the VM name
 $vm = Read-Host -Prompt "Paste the VM name";
 
 #Enter the resource group name
 $rgname = Read-Host -Prompt "Enter the resource group name";
+
+#Enter the port number
+$portnumber = Read-Host -Prompt "Enter the port number";
 
 # Get NSGs from NIC and Subnet
 $vmObject = Get-AzVM -Name $vm -ResourceGroupName $rgname;
@@ -13,12 +16,9 @@ $nicNSG = (Get-AzResource -ResourceId $nic.NetworkSecurityGroup.Id).Name;
 $vnet = Get-AzVirtualNetwork -ResourceGroupName $rgname;
 $subnet = $vnet.Subnets[0];
 $subnetNSG = (Get-AzResource -ResourceId $subnet.NetworkSecurityGroup.Id).Name;
- 
-#Enter the port number
-$portnumber="3389";
- 
+  
 # Get your IP Address
-$myipaddress=(Invoke-WebRequest https://myexternalip.com/raw).content;
+$myipaddress = (Invoke-WebRequest https://myexternalip.com/raw).content;
 
 # Add NSGs to array
 $NSGArray = $nicNSG, $subnetNSG;
